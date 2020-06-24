8d1c62dcdf606134d5d8bfeed06a8974b26b393a
DivBear03 <56279449+DivBear03@users.noreply.github.com>
2020-06-23 20:54:28 -0400

    8:54 PM

--- Generalization.py
+++ Generalization.py
@@ -1,11 +1,15 @@
+import difflib
+from difflib import SequenceMatcher
+import numpy as np
+import itertools
+
 texthandle = open("Aircraft - Allied.txt", 'r')
 plane = input("Please enter the plane name: ")
-terms = plane.split(" ")
-print(terms)
-possibilities = []
-for n in range(len(terms)):    
-    for line in texthandle:
-        if line.lower().startswith(terms[0]):
-            possibilities.append(line)
-for thing in possibilities:
-    print(thing)
+alliedaircraft = []
+similarities = {}
+for line in texthandle:
+    alliedaircraft.append(line.lower())
+for plane1 in alliedaircraft:
+    similarity = difflib.SequenceMatcher(None, plane, plane1).ratio()
+    similarities[plane1] = similarity * 100
+
