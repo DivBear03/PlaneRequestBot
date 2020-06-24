857f306cfcc3cddc3855a60eaae72374253b58d0
DivBear03 <56279449+DivBear03@users.noreply.github.com>
2020-06-23 21:00:14 -0400

    9:00 PM

--- Generalization.py
+++ Generalization.py
@@ -2,14 +2,27 @@ import difflib
 from difflib import SequenceMatcher
 import numpy as np
 import itertools
-
-texthandle = open("Aircraft - Allied.txt", 'r')
-plane = input("Please enter the plane name: ")
 alliedaircraft = []
-similarities = {}
+texthandle = open("Aircraft - Allied.txt", 'r')
 for line in texthandle:
     alliedaircraft.append(line.lower())
-for plane1 in alliedaircraft:
-    similarity = difflib.SequenceMatcher(None, plane, plane1).ratio()
-    similarities[plane1] = similarity * 100
+while True:
+    similarities = {}
+    plane = input("Please enter the plane name: ")
+    for plane1 in alliedaircraft:
+        similarity = difflib.SequenceMatcher(None, plane, plane1).ratio()
+        similarities[plane1] = similarity * 100
 
+    sortedlist = list()                 #creating empty list to hold sorted users
+    for thing in similarities.items():     #iterate through the keys and terms of usercount dictionary
+        sortedlist.append(thing)        #add each key,value pair to sortedlist
+    for i in range(1, len(sortedlist)):         #insertion sort algorithm
+        nextElementValue = sortedlist[i][1]
+        temp = sortedlist[i]
+        j = i-1
+        while j >= 0 and sortedlist[j][1] < nextElementValue:
+            item = sortedlist[j]
+            sortedlist[j+1] = item
+            j = j-1
+        sortedlist[j+1] = temp
+    print(sortedlist[0])
