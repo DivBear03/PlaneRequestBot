29c6417cd21ebf1728698cf031e66a5c24b856d0
DivBear03 <56279449+DivBear03@users.noreply.github.com>
2020-06-27 12:24:20 -0400

    s

--- Searcher.py
+++ Searcher.py
@@ -29,103 +29,108 @@ for line in bombhandle:
 def time_convert(time_diff):                                                  #function for converting seconds into a readable time
     execution_time = time_diff.total_seconds()
     return execution_time
-def search(plane):                                                      #search algorithm
-    
 
-    def cleanup2(plane):                                    #function for cleaning up whitespace and non-alpha-numeric characters
-        plane = plane.replace("-", "")
-        plane = plane.replace(" ", "")
-        plane = plane.lower()
-        plane = plane.replace("\n", "")
-        plane = plane.replace("(", "")
-        plane = plane.replace(")", "")
-        return plane
+def cleanup2(plane):                                    #function for cleaning up whitespace and non-alpha-numeric characters
+    plane = plane.replace("-", "")
+    plane = plane.replace(" ", "")
+    plane = plane.lower()
+    plane = plane.replace("\n", "")
+    plane = plane.replace("(", "")
+    plane = plane.replace(")", "")
+    return plane
+
+def gestalt(plane,plane1):
+    return (100 * difflib.SequenceMatcher(None, plane, cleanup2(plane1)[:len(plane)+1]).ratio())
+
+def defSim(plane, plane1):
+    return gestalt(plane, plane1)
+
+def inSim(plane,plane1):
+    if plane in cleanup2(plane1):
+        return 100
+    else:
+        return defSim(plane,plane1)
+
+def isSim(plane, plane1):
+    if plane == cleanup2(plane1):
+        return 110
+    else:
+        return defSim(plane,plane1)
+
+def orderSim():
+    pass
+    
+def compSim():
+    pass
+    
+def mineditDist(plane,plane1):
+    plane1 = cleanup2(plane1)
+    len1 = len(plane) +1
+    len2 = len(plane1) +1    
+    matrix = [[0 for x in range(len1+1)] for y in range(len2+1)]
+        
+    for x in range(len2+1):
+        matrix[x][0] = x
+    for y in range(len1+1):
+        matrix[0][y] = y
+
+    for xx in range(1,len2):
+        for yy in range(1,len1):
+                
+            if plane1[xx-1] == plane[yy-1]:
+                matrix[xx][yy] = matrix[xx-1][yy-1]
+            else:
+                matrix[xx][yy] = min([matrix[xx-1][yy],matrix[xx][yy-1],matrix[xx-1][yy-1]]) + 1
+                #print(xx,yy,matrix[xx][yy])   
+    dist = matrix[len2-1][len1-1]
+        
+    ret = scipy.stats.norm(0,3).pdf(dist)/scipy.stats.norm(0,3).pdf(0)
+
+    return ret*100
+
+def search2(plane):
     
     plane = cleanup2(plane)
+
+    if len(plane) <= 2:
+        return "No match"
 
     for plane1 in bombers:
         substring = cleanup2(plane1)[:len(plane)]
         if plane in substring:
             return "Bombers are useless"
-    
-    def gestalt(plane,plane1):
-        return (100 * difflib.SequenceMatcher(None, plane, cleanup2(plane1)[:len(plane)+1]).ratio())
-    
-    def defSim(plane, plane1):
-        return gestalt(plane, plane1)
-    
-    def inSim(plane,plane1):
-        if plane in cleanup2(plane1):
-            return 100
-        else:
-            return defSim(plane,plane1)
-    
-    def isSim(plane, plane1):
-        if plane == cleanup2(plane1):
-            return 110
-        else:
-            return defSim(plane,plane1)    
-    
-    def orderSim():
-        pass
-    
-    def compSim():
-        pass
-    
-    def mineditDist(plane,plane1):
-        plane1 = cleanup2(plane1)
-        len1 = len(plane) +1
-        len2 = len(plane1) +1
-        
-        matrix = [[0 for x in range(len1+1)] for y in range(len2+1)]
-        
-        for x in range(len2+1):
-            matrix[x][0] = x
-        for y in range(len1+1):
-            matrix[0][y] = y
-        
-        for xx in range(1,len2):
-            for yy in range(1,len1):
-                
-                if plane1[xx-1] == plane[yy-1]:
-                    matrix[xx][yy] = matrix[xx-1][yy-1]
-                else:
-                    matrix[xx][yy] = min([matrix[xx-1][yy],matrix[xx][yy-1],matrix[xx-1][yy-1]]) + 1
-                #print(xx,yy,matrix[xx][yy])   
-        dist = matrix[len2-1][len1-1]
-        
-        ret = scipy.stats.norm(0,3).pdf(dist)/scipy.stats.norm(0,3).pdf(0)
-
-        return ret*100
-                    
-                    
-                    
+  
     similarities = {}                                       #dictionary for holding all the planes and their respective match percentages
     plane = cleanup2(plane)
 
-    # foreign = False
-    # if "usa" in plane or "ussr" in plane or 'japan' in plane or 'germany' in plane or 'italy' in plane or 'china' in plane or 'france' in plane or 'greatbritain' in plane:
-    #     foreign = True
+    foreign = False
+    if "usa" in plane or "ussr" in plane or 'japan' in plane or 'germany' in plane or 'italy' in plane or 'china' in plane or 'france' in plane or 'greatbritain' in plane:
+        foreign = True
 
 
-    #if foreign == False:
-    for plane1 in aircraft:
-        sim = defSim(plane, plane1)
-        sim1 = isSim(plane,plane1)
-        sim2 = inSim(plane,plane1)
-        sim3 = mineditDist(plane, plane1)
-        plane1 = plane1.replace("\n", "")            
-        similarities[plane1] = (sim + sim1 + sim2 + .5*sim3)/4                                             
+    if foreign == False:
         
-    # else:
-    #     for plane1 in aircraft:
-    #         if plane == cleanup2(plane1):
-    #             similarities[plane1] = 100
-    #             break
-    #         elif "[" in plane1 and len(plane) <= len(cleanup2(plane1)):
-    #             similarity = gestalt(plane, plane1)
-    #             plane1 = plane1.replace("\n", "")
-    #             similarities[plane1] = similarity * 100
+        for plane1 in aircraft:
+                
+            sim1 = isSim(plane,plane1)       
+            sim = defSim(plane, plane1)
+            if sim < 60:
+                similarities[plane1] = sim
+                continue
+            sim2 = inSim(plane,plane1)
+            sim3 = mineditDist(plane, plane1)
+            plane1 = plane1.replace("\n", "")            
+            similarities[plane1] = (sim + sim1 + sim2 + .5*sim3)/4                                             
+        
+    else:
+        for plane1 in aircraft:
+            if plane == cleanup2(plane1):
+                similarities[plane1] = 100
+                break
+            elif "[" in plane1 and len(plane) <= len(cleanup2(plane1)):
+                similarity = gestalt(plane, plane1)
+                plane1 = plane1.replace("\n", "")
+                similarities[plane1] = similarity * 100
 
 
     sortedlist = list()                 #creating empty list to hold sorted users
@@ -155,15 +160,12 @@ def search(plane):                                                      #search 
         if len(samesims[n][0]) < shortest:          #if the present plane has a shorter string length
             shortest = len(samesims[n][0])          #make the shortest length to be that length
             shortestindex = n                       #set the index of the shortest string length to be that index
-    if samesims[shortestindex][1] > 55:             #if the match found is reasonably comparable to the request
+    if samesims[shortestindex][1] > 60:             #if the match found is reasonably comparable to the request
         return samesims[shortestindex]              #return the plane with the highest match
     else:
         return "No match"
-    
-    
-    
-    
-    
+
+
 while True:
     a = input("Search for:")
     if "end" in a:
